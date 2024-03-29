1c68d7e0b76c46c173831ccc31def72750390e47
AirtonPatrocinioJR <airtonpatrocinio.jr@gmail.com>
2024-03-28 23:19:19 -0300

    get env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,19 +7,26 @@ import { PagamentoModule } from './pagamento/pagamento.module';
 import { PedidoModule } from './pedido/pedido.module';
 import { Pagamento } from './pagamento/pagamento.entity/pagamento.entity';
 import { Pedido } from './pedido/pedido.entity/pedido.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as 'mysql',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      autoLoadEntities: true,
-      synchronize: true,
-      entities: [Pagamento, Pedido],
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna o ConfigModule disponível globalmente
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql', // ou outro banco de dados
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'), // o operador '+' converte a string para número
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities:  [Pagamento, Pedido],
+        // outras configurações necessárias...
+      }),
     }),
     PagamentoModule,
     PedidoModule,
